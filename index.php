1691bf71245dd0bf19fee2a1a23fa9b6c24fcada
Airat Mufazalov <airat.mufazalov@live.ru>
2017-08-22 19:30:52 +0300

    Подключили bootstrap

--- index.php
+++ index.php
@@ -1,39 +1,69 @@
 
-<!doctype html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
+
 
 <?php
 //echo "Обработчик формы";
 //var_dump($_REQUEST);
 $message =false;
-if
-(isset( $_REQUEST['name']) and isset($_REQUEST['phone']))
+$error =false;
+if (isset( $_REQUEST['name']) and isset($_REQUEST['phone']))
+
 {   $name = $_REQUEST["name"];
     $phone = $_REQUEST["phone"];
-    $row = 'Здравствуйте,' . $name .
-        '  Ваш номер:' . $phone. PHP_EOL;
-    file_put_contents('./contacts.txt', $row, FILE_APPEND);
-    $message ='Спасибо, Мы с Вами свяжемся';
+    if( empty($name)  or empty($phone)) {
+
+    } else{
+        $row = 'Здравствуйте,' . $name .
+            '  Ваш номер:' . $phone. PHP_EOL;
+        file_put_contents('./contacts.txt',
+            $row, FILE_APPEND);
+
+        $message ='Спасибо, Мы с Вами свяжемся';
+    }
 }
 ?>
-
-
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="./Css/bootstrap.min.css"
+</head>
+<body>
+<div class="container"
+     <h1>Форма обратной связи</h1>
 <?php if ($message ) : ?>
 <?= $message?>
-<php else: ?>
-<form action="index.php" method="post">
-<p>Представтесь</p>
-<input type="text" name="name">
-<p> Укажите Ваш номер</p>
-<input type="text" name="phone">
-<button type="submit">Отправить</button>
-
-</form>
-    <?php endif; ?>
+<?php else: ?>
+    <form class="form-horizontal" role="form">
+        <div class="form-group">
+            <label for="inputEmail3" class="col-sm-4 control-label">ФИО</label>
+            <div class="col-sm-4">
+                <input type="text" name="name" class="form-control" >
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="inputPassword3" class="col-sm-4 control-label">Номер телефона</label>
+            <div class="col-md-4">
+                <input type="password" name="phone" class="form-control" ">
+            </div>
+        </div>
+        <!--    <div class="form-group">-->
+        <!--        <div class="col-sm-offset-2 col-sm-10">-->
+        <!--            <div class="checkbox">-->
+        <!--                <label>-->
+        <!--                    <input type="checkbox"> Запомнить меня-->
+        <!--                </label>-->
+        <!--            </div>-->
+        <!--        </div>-->
+        <!--    </div>-->
+        <!--    <div class="form-group">-->
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="alert-danger">Отправить</button>
+        </div>
+        </div>
+        <p class="alert-danger col-md-4"><?= $error ?></p>
+    </form>
+<?php endif; ?>
 </body>
 </html>
