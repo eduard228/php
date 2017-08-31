c8abdd6f47dda005059007f92ed5697ab94cfe1b
Airat Mufazalov <airat.mufazalov@live.ru>
2017-08-31 19:59:13 +0300

    Урок 6

--- index.php
+++ index.php
@@ -1,69 +1,124 @@
-
-
-
 <?php
-//echo "Обработчик формы";
-//var_dump($_REQUEST);
-$message =false;
-$error =false;
-if (isset( $_REQUEST['name']) and isset($_REQUEST['phone']))
 
-{   $name = $_REQUEST["name"];
-    $phone = $_REQUEST["phone"];
-    if( empty($name)  or empty($phone)) {
+$message = false; //
+$error = false; // Для ошибок
 
-    } else{
-        $row = 'Здравствуйте,' . $name .
-            '  Ваш номер:' . $phone. PHP_EOL;
-        file_put_contents('./contacts.txt',
-            $row, FILE_APPEND);
+if (isset($_REQUEST['name']) and isset($_REQUEST['phone']) and isset($_REQUEST['surname']) and isset($_REQUEST['email'])) {
 
-        $message ='Спасибо, Мы с Вами свяжемся';
+    $name = $_REQUEST['name']; // Для сокращения
+    $surname = $_REQUEST['surname'];
+    $phone = $_REQUEST['phone'];
+    $email = $_REQUEST['email'];
+
+
+    if (empty($name) || empty($phone) or empty($surname) or empty($email)) {
+        $error = '<strong>Заполните, пожалуйста поля</strong>';
+    } else {
+        $row = 'Здравствуйте, ' . $name . ' ' .
+            $surname .
+            '. Ваш номер: ' . $phone .
+            '. Вам:' . ' лет' .
+            '. Ваш email:' . $email .
+            '.Ваш рассказ:' .  PHP_EOL;
+
+
+// PHP_EOL = '\n'
+
     }
+
 }
+
 ?>
+
 <!doctype html>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <link rel="stylesheet" href="./Css/bootstrap.min.css"
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
 <body>
-<div class="container"
-     <h1>Форма обратной связи</h1>
-<?php if ($message ) : ?>
-<?= $message?>
-<?php else: ?>
-    <form class="form-horizontal" role="form">
-        <div class="form-group">
-            <label for="inputEmail3" class="col-sm-4 control-label">ФИО</label>
-            <div class="col-sm-4">
-                <input type="text" name="name" class="form-control" >
+
+<div class="container">
+    <h3>Анкета</h3>
+
+    <?php if ($message) : ?>
+        <p class="alert-success col-md-4"><?= $message ?></p>
+    <?php else: ?>
+        <form class="form-horizontal" action="index.php" method="post">
+            <div class="form-group">
+                <label for="name" class="col-sm-2 control-label">Имя</label>
+                <div class="col-md-4">
+                    <input type="text" name="name" class="form-control" placeholder="Имя" >
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label for="inputPassword3" class="col-sm-4 control-label">Номер телефона</label>
-            <div class="col-md-4">
-                <input type="password" name="phone" class="form-control" ">
+
+            <div class="form-group">
+                <label for="name" class="col-sm-2 control-label">Фамилия</label>
+                <div class="col-md-4">
+                    <input type="text" name="surname" class="form-control" placeholder="Фамилия" >
+                </div>
             </div>
-        </div>
-        <!--    <div class="form-group">-->
-        <!--        <div class="col-sm-offset-2 col-sm-10">-->
-        <!--            <div class="checkbox">-->
-        <!--                <label>-->
-        <!--                    <input type="checkbox"> Запомнить меня-->
-        <!--                </label>-->
-        <!--            </div>-->
-        <!--        </div>-->
-        <!--    </div>-->
-        <!--    <div class="form-group">-->
-        <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="alert-danger">Отправить</button>
-        </div>
-        </div>
-        <p class="alert-danger col-md-4"><?= $error ?></p>
-    </form>
-<?php endif; ?>
+
+            <div class="form-group">
+                <label for="phone" class="col-sm-2 control-label">Номер телефона</label>
+                <div class="col-md-4">
+                    <input type="phone" name="phone" class="form-control" placeholder="Введите номер" >
+                </div>
+            </div>
+
+
+
+            <div class="form-group">
+                <label for="name" class="col-sm-2 control-label">Email</label>
+                <div class="col-md-4">
+                    <input type="email" name="email" class="form-control" id="exampleInputEmail2" placeholder="email" >
+                </div>
+            </div>
+<p>Возраст</p>
+            <input type="number" name="age">
+            <br>
+
+<p1>О себе</p1> <br>
+            <textarea name="about" id="about" cols="30" rows="10"></textarea> <br>
+
+            <label for="interests1">Ваши интересы</label><br>
+            <input type="checkbox" name = "interests1" value="comp">Техника<br>
+            <input type="checkbox" name = "interests2" value="comp">Спорт<br>
+            <input type="checkbox" name = "interests1" value="comp">Искусство<br>
+            <input type="checkbox" name = "interests1" value="comp">Наука<br>
+            <input type="checkbox" name = "interests1" value="comp">Автомобили<br>
+            <br>
+            <p>Пол</p>
+            <input type="radio" name="gender" value="male"> Мужской
+            <input type="radio" name="gender" value="female">Женский
+
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-md-10">
+                    <button type="submit" class="btn btn-primary">Отправить</button>
+                </div>
+            </div>
+            <p class="alert-danger col-md-4"><?= $error ?></p>
+
+
+
+        </form>
+    <?php endif; ?>
+
+
+</div>
+
 </body>
 </html>
+
+
+<?php
+
+function fieldIsset($field){
+    if (isset($field) && empty($field)){
+        return true;
+    }
+    return false;
+}
+
+
